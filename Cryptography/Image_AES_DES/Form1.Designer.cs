b40ffd442830ab4c958b454aa9f8202cbdba019d
Agustin Galindo Reyes <38082376+agus2207@users.noreply.github.com>
2020-07-03 19:12:31 -0500

    Add files via upload

---
+++ Cryptography/Image_AES_DES/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Image_AES_DES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(102, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(534, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to AES/DES Encrypt/Decrypt program";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select a file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(210, 336);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "AES ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(408, 336);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "DES";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(245, 128);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(209, 166);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(188, 375);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(131, 21);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Encrypt/Decrypt";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(381, 375);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(131, 21);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "Encrypt/Decrypt";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 458);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Cryptography/Image_AES_DES/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Image_AES_DES
+{
+    public partial class Form1 : Form
+    {
+        string sFileName;
+        string skey;
+        string destino;
+        Image img;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog selector = new OpenFileDialog();
+            selector.Filter = "All Files (*.*)|*.*";
+            selector.FilterIndex = 1;
+            selector.Multiselect = true;
+
+            if (selector.ShowDialog() == DialogResult.OK)
+            {
+                sFileName = selector.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)//if checked, uncheck it
+            {
+                checkBox1.Checked = false;
+            }
+            else // if unchecked, check it
+            {
+                checkBox1.Checked = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            skey = GenerateKey();   
+            if (checkBox2.Checked)
+            {
+                destino = @"../../imagen.bmp";
+                EncryptFile(sFileName, destino, skey);
+            }
+            else // if unchecked, check it
+            {
+                //sFileName = @"../../imagen.bmp";
+                destino = @"../../imagen2.bmp";
+                DecryptFile(sFileName, destino, skey);
+            }
+        }
+
+        private void EncryptFile(string source, string destination, string sKey)
+        {
+            FileStream fsInput = new FileStream(source,
+                FileMode.Open,
+                FileAccess.Read);
+
+            FileStream fsEncrypted = new FileStream(destination,
+                            FileMode.Create,
+                            FileAccess.Write);
+
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+            DES.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+            DES.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
+            ICryptoTransform desencrypt = DES.CreateEncryptor();
+            CryptoStream cryptostream = new CryptoStream(fsEncrypted,
+                                desencrypt,
+                                CryptoStreamMode.Write);
+            //byte[] bytearrayinput = new byte[fsInput.Length - 1];
+            byte[] headerBuffer = new byte[54];
+            fsInput.Read(headerBuffer, 0, headerBuffer.Length);
+            fsEncrypted.Write(headerBuffer, 0, headerBuffer.Length);
+            //cryptostream.Write(bytearrayinput, 0, bytearrayinput.Length);
+            fsInput.CopyTo(cryptostream);
+            cryptostream.Close();
+            fsInput.Close();
+            fsEncrypted.Close();
+
+        }
+
+        private string GenerateKey()
+        {
+            // Create an instance of Symetric Algorithm. Key and IV is generated automatically.
+            DESCryptoServiceProvider desCrypto = (DESCryptoServiceProvider)DESCryptoServiceProvider.Create();
+
+            // Use the Automatically generated key for Encryption. 
+            return ASCIIEncoding.ASCII.GetString(desCrypto.Key);
+
+        }
+
+        private void DecryptFile(string source, string destination, string sKey)
+        {
+            try
+            {
+                FileStream fsInput = new FileStream(source, FileMode.Open, FileAccess.Read);
+
+                FileStream fsDecrypted = new FileStream(destination, FileMode.Create, FileAccess.Write);
+
+                DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+                DES.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+                DES.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
+
+                ICryptoTransform desdecrypt = DES.CreateDecryptor();
+                CryptoStream cryptostream = new CryptoStream(fsDecrypted, desdecrypt, CryptoStreamMode.Read);
+
+                byte[] headerBuffer = new byte[54];
+
+                fsInput.Read(headerBuffer, 0, headerBuffer.Length);
+
+                fsDecrypted.Write(headerBuffer, 0, headerBuffer.Length);
+                fsInput.CopyTo(cryptostream);
+
+                cryptostream.Close();
+                fsInput.Close();
+                fsDecrypted.Close();
+            }catch(Exception e)
+            {
+                
+            }
+        }
+    }
+}
